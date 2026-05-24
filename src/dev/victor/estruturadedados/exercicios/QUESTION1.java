8e0d397bd7b1c8608af3033d0a60159072694a79
Victor Gabriel <vitinmoreira1908@icloud.com>
2026-05-24 15:34:09 -0300

    Refactor comments and formatting in QUESTION1.java

--- src/dev/victor/estruturadedados/exercicios/QUESTION1.java
+++ src/dev/victor/estruturadedados/exercicios/QUESTION1.java
@@ -4,7 +4,7 @@ public class QUESTION1 {
     public void addAscendingSorted(T element) {
         Node<T> newNode = new Node<>(element);
 
-        // Trata lista vazia ou inserção no início (novo valor é o menor)
+        //trata lista vazia
         if (head == null || ((Comparable<T>) head.element).compareTo(element) > 0) {
             newNode.next = head;
             head = newNode;
@@ -13,12 +13,12 @@ public class QUESTION1 {
 
         Node<T> current = head;
 
-        // Avança enquanto o prximo no for nulo E o valor do prximo for menorou igual ao novo
+        //avança enquanto o prximo no for nulo E o valor do prximo for menorou igual ao novo
         while (current.next != null && ((Comparable<T>) current.next.element).compareTo(element) <= 0) {
             current = current.next;
         }
 
-        // Insere o newNode entre current e current.next
+        // coloca o newNode entre current e current.next
         newNode.next = current.next;
         current.next = newNode;
     }
