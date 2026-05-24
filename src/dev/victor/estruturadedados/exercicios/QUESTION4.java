4f4246c5162c540874a0ceb7e8de2d6abd67afd5
Victor Gabriel <vitinmoreira1908@icloud.com>
2026-05-24 15:35:03 -0300

    Refactor QUESTION4.java formatting and structure

--- src/dev/victor/estruturadedados/exercicios/QUESTION4.java
+++ src/dev/victor/estruturadedados/exercicios/QUESTION4.java
@@ -2,11 +2,9 @@ package dev.victor.estruturadedados.exercicios;
 
 public class QUESTION4 {
     public Node<T> mergeTwoLists(Node<T> init1, Node<T> init2) {
-        // Nó "falso" para servir de ponto de partida
         Node<T> init = new Node<>(null);
         Node<T> current = init;
 
-        // Percorre enquanto ambas tiverem elementos
         while (init1 != null && init2 != null) {
             if (((Comparable<T>) init1.element).compareTo(init2.element) <= 0) {
                 current.next = init1;
@@ -18,14 +16,12 @@ public class QUESTION4 {
             current = current.next;
         }
 
-        // Se uma das listas terminou, anexa o restante da outra
         if (init1 != null) {
             current.next = init1;
         } else {
             current.next = init2;
         }
 
-        // Retorna ignorando o nó falso de inicialização
         return init.next;
     }
 }
